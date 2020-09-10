a050d9457afc760e287d582b33181b514c38688f
canojayson10 <cano.jayson10@gmail.com>
2020-09-10 11:28:30 +0800

    update and remove test comment

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -24,10 +24,6 @@ export default function dashboard() {
       <Navmenu />
       <div className='section-main dashboard card-widget'>
         <div className='container'>
-          <div className='row'>
-            <div className="col-lg-12">
-              test
-            </div>
             <div className='col-lg-9'>
               <div className='row'>
                 <div className='col-lg-4'>
